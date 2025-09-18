93b5222d448e492053ba7720c909ef9063132729
Syhatt <farhatmuafy27@gmail.com>
2025-09-19 01:39:02 +0700

    fixing dynamic form & fixing store data entries with library compress image (intervention)

--- resources/views/entry/forms/_surgical.blade.php
+++ resources/views/entry/forms/_surgical.blade.php
@@ -1,39 +1,82 @@
+{{-- ======================== --}}
+{{-- Surgical Care Form --}}
+{{-- ======================== --}}
+<h5 class="mt-3">Form Surgical Care</h5>
+
+<div class="mb-3">
+    <label>Entry Key</label>
+    <input type="text" name="entry_key" class="form-control">
+</div>
+
 <div class="mb-3">
     <label>Tanggal Operasi</label>
     <input type="date" name="surgical_date" class="form-control">
 </div>
+
 <div class="mb-3">
     <label>Lokasi Operasi</label>
     <input type="text" name="surgical_site_key" class="form-control">
 </div>
-<div class="mb-3">
-    <label>Jam Mulai</label>
-    <input type="time" name="surgery_start_time" class="form-control">
+
+<div class="row">
+    <div class="col-md-6 mb-3">
+        <label>Jam Mulai</label>
+        <input type="time" name="surgery_start_time" class="form-control">
+    </div>
+    <div class="col-md-6 mb-3">
+        <label>Jam Selesai</label>
+        <input type="time" name="surgery_end_time" class="form-control">
+    </div>
 </div>
+
+@for ($i = 1; $i <= 4; $i++)
+    <div class="mb-3">
+        <label>Operator {{ $i }}</label>
+        <input type="text" name="operator_{{ $i }}" class="form-control">
+    </div>
+@endfor
+
 <div class="mb-3">
-    <label>Jam Selesai</label>
-    <input type="time" name="surgery_end_time" class="form-control">
-</div>
-<div class="mb-3">
-    <label>Operator 1</label>
-    <input type="text" name="operator_1" class="form-control">
-</div>
-<div class="mb-3">
-    <label>Operator 2</label>
-    <input type="text" name="operator_2" class="form-control">
-</div>
-<div class="mb-3">
-    <label>Diagnosis Pre</label>
+    <label>Preoperative Diagnosis</label>
     <textarea name="preoperative_diagnosis" class="form-control"></textarea>
 </div>
-<div class="mb-3"><label>Diagnosis Intra</label>
+
+<div class="mb-3">
+    <label>Intraoperative Diagnosis</label>
     <textarea name="intraoperative_diagnosis" class="form-control"></textarea>
 </div>
-<div class="mb-3"><label>Tindakan</label>
+
+<div class="mb-3">
+    <label>Tindakan Operasi</label>
     <textarea name="surgical_procedure" class="form-control"></textarea>
 </div>
-<div class="mb-3"><label>Kehilangan Darah</label><input type="number" name="estimated_blood_loss"
-        class="form-control"></div>
-<div class="mb-3"><label>Catatan</label>
+
+<div class="mb-3">
+    <label>Perkiraan Kehilangan Darah (ml)</label>
+    <input type="number" name="estimated_blood_loss" class="form-control">
+</div>
+
+<div class="mb-3">
+    <label>Catatan Operasi</label>
     <textarea name="surgical_notes" class="form-control"></textarea>
+</div>
+
+<div class="mb-3">
+    <label>Supervisor</label>
+    <input type="text" name="entry_supervisor" class="form-control">
+</div>
+
+<div class="mb-3">
+    <label>Tingkat Kompetensi</label>
+    <input type="text" name="competence_level" class="form-control">
+</div>
+
+<div class="mb-3">
+    <label>Status Asuransi</label>
+    <input type="text" name="insurance_status" class="form-control">
+</div>
+
+<div class="mb-3">
+    <label>Catatan Asuransi</label>
+    <textarea name="insurance_notes" class="form-control"></textarea>
 </div>
--- resources/views/entry/forms/_waitlist_active.blade.php
+++ resources/views/entry/forms/_waitlist_active.blade.php
@@ -1,12 +1,22 @@
+{{-- ACTIVE --}}
+<h5 class="mt-3">Surgical Waitlist - Active</h5>
+
 <div class="mb-3">
     <label>Tanggal Masuk</label>
     <input type="date" name="waitlist_entry_date" class="form-control">
 </div>
+
 <div class="mb-3">
     <label>Group</label>
     <input type="text" name="waitlist_group" class="form-control">
 </div>
+
 <div class="mb-3">
-    <label>Urgensi</label>
+    <label>Tipe / Urgensi</label>
     <input type="text" name="waitlist_type" class="form-control">
+</div>
+
+<div class="mb-3">
+    <label>Durasi</label>
+    <input type="text" name="waitlist_duration" class="form-control">
 </div>
